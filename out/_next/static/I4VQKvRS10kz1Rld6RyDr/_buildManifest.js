e56200cfc6e530da146d49a743c5763632e36df0
unknown <hamid2004main@gmail.com>
2023-06-18 12:44:55 +0430

    initial commit

---
+++ out/_next/static/I4VQKvRS10kz1Rld6RyDr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b4530d75-222d8e7691bfc855.js","static/chunks/pages/index-2a75d64bea975487.js"],"/_error":["static/chunks/pages/_error-82b79221b9ed784b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
